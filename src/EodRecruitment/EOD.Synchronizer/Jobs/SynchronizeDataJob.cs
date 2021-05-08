56a70e0325a4fa4531f1f734bf281e1a40eb7de4
Mateusz Wais <s19251@pjwstk.edu.pl>
2021-05-08 21:58:45 +0200

    Dodanie pustego joba do synchronizacji danych

---
+++ src/EodRecruitment/EOD.Synchronizer/Jobs/SynchronizeDataJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOD.Synchronizer.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class SynchronizeDataJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Uruchomiono joba - {DateTime.Now}");
+            });
+        }
+    }
+}
--- src/EodRecruitment/EOD.Synchronizer/Service/EodSynchronizerService.cs
+++ src/EodRecruitment/EOD.Synchronizer/Service/EodSynchronizerService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EOD.Synchronizer.Jobs;
+using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +11,39 @@ namespace EOD.Synchronizer.Service
 {
     public class EodSynchronizerService
     {
+        private readonly IScheduler _scheduler;
+
+        public EodSynchronizerService()
+        {
+            _scheduler = new StdSchedulerFactory().GetScheduler().GetAwaiter().GetResult();
+        }
+
         public void Start()
         {
             Console.WriteLine("Uruchomiono usługe EOD Synchronizer");
+
+            _scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<SynchronizeDataJob>()
+                .WithIdentity("SynchronizeDataJob", "SynchronizerGroup")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("SynchronizerTrigger", "SynchronizerGroup")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(30)
+                    .RepeatForever())
+                .Build();
+
+            _scheduler.ScheduleJob(job, trigger);
         }
 
         public void Stop()
         {
             Console.WriteLine("Zatrzymywanie usługi EOD Synchronizer");
+            
+            _scheduler.Shutdown(true);
         }
     }
 }
