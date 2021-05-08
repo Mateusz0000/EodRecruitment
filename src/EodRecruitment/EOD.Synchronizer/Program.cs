609340a05e742ae33f500e32b17607b30ea75b96
Mateusz Wais <s19251@pjwstk.edu.pl>
2021-05-08 20:45:22 +0200

    Dodanie projektu aplikajci konsolowej z usługą Topshelf do synchronizacji danych pomiędzy ERP a bazą EOD

---
+++ src/EodRecruitment/EOD.Synchronizer/Program.cs
@@ -0,0 +1,30 @@
+﻿using EOD.Synchronizer.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace EOD.Synchronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<EodSynchronizerService>(service =>
+                {
+                    service.ConstructUsing(s => new EodSynchronizerService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("EODSynchronizer");
+                configure.SetDisplayName("EOD Synchronizer");
+                configure.SetDescription("Usługa synchronizująca dane pomiędzy bazami ERP i EOD.");
+            });
+        }
+    }
+}
---
+++ src/EodRecruitment/EOD.Synchronizer/Service/EodSynchronizerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOD.Synchronizer.Service
+{
+    public class EodSynchronizerService
+    {
+        public void Start()
+        {
+            Console.WriteLine("Uruchomiono usługe EOD Synchronizer");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Zatrzymywanie usługi EOD Synchronizer");
+        }
+    }
+}
